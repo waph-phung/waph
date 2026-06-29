2b93529acf3bc958cd353829d5c89947db9fa512
Phu Phung <phung-waph@hotmail.com>
2026-06-29 22:37:38 +0000

    Lab 4 code skeleton

---
+++ labs/lab4/sessiontest.php
@@ -0,0 +1,9 @@
+<?php
+
+if(isset($_SESSION['views']))
+    $_SESSION['views'] = $_SESSION['views']+ 1;
+else{
+    $_SESSION['views'] = 1;
+}
+echo "You have visited this page " . $_SESSION['views'] . " times"; 
+?>
